3bc20df43530f199d4b16eb7f69b33328a1dd3f4
Mikhailov Alexey <55998890+Saviorich@users.noreply.github.com>
2020-11-26 20:43:59 +0300

    Update TimeParser.java

--- jwd-task01/src/main/java/by/epam/tc/task01/t06/logic/TimeParser.java
+++ jwd-task01/src/main/java/by/epam/tc/task01/t06/logic/TimeParser.java
@@ -6,12 +6,12 @@ public class TimeParser {
     private static final int SECONDS_IN_DAY = 86400;
 
     public int[] getPassedDayTime(int seconds) {
-        if (seconds <= SECONDS_IN_DAY) {
+        if (seconds <= SECONDS_IN_DAY && seconds >= 0) {
             int hours = seconds / SECONDS_IN_HOUR;
             int minutes = (seconds - hours * SECONDS_IN_HOUR) / SECONDS_IN_MINUTE;
             seconds -= (hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE);
             return new int[]{hours, minutes, seconds};
         }
-        return null;
+        return new int[0];
     }
 }
